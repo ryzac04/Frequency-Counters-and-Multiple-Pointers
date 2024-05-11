d0d6d8077850632d6059eb84066e584c0872e5aa
ryzac <rzc044@gmail.com>
2024-05-11 10:50:17 -0500

    two-array-object.js complete and passes all tests

--- two-array-object.js
+++ two-array-object.js
@@ -1,5 +1,19 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+
+// O(n)
+function twoArrayObject(keys, values) {
+    let obj = {};
+    const maxLength = Math.min(keys.length, values.length);
+
+    for (let i = 0; i < maxLength; i ++) {
+        obj[keys[i]] = values[i];
+    }
+
+    for (let i = maxLength; i < keys.length; i++){
+        obj[keys[i]] = null;
+    }
+
+    return obj;
 }
 
 module.exports = twoArrayObject;
